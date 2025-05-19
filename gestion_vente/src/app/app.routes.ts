81d8d1ae3ca9df5ee2c4291fbebf7633c7d8d07e
Cipo <cipokibona.ck@gmail.com>
2025-05-19 16:20:54 +0200

    creation de login component

--- gestion_vente/src/app/app.routes.ts
+++ gestion_vente/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    // { 
-    //     path: '',
-    //     redirectTo: '/login',
-    //     pathMatch: 'full'
-    // },
+    { 
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path:'login',
+        title: 'Login Page',
+        component: LoginComponent,
+    },
 ];
